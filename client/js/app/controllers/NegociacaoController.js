5b43c849f5163bdb64c5da7230709265357ff3ba
brunobc <brunobezerrachaves@gmail.com>
2017-01-19 07:56:53 -0300

    Aula 4 - Template Strings

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,16 +13,12 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let data = new Date(...this._inputData
-          .value.split('-')
-          .map((item, indice) => item - indice % 2));
+        let negociacao =
+          new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+          );
 
-        let negociacao = new Negociacao(
-          data,
-          this._inputQuantidade.value,
-          this._inputValor.value
-        );
-
-        console.log(negociacao);
     }
 }
