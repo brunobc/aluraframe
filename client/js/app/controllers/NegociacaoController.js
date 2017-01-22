22bab481e761b3479509d8ac418b93b010c2106c
brunobc <brunobezerrachaves@gmail.com>
2017-01-22 14:56:59 -0300

    JS II Aula 3 - Factory

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,63 +2,45 @@ class NegociacaoController {
 
     constructor() {
         let $ = document.querySelector.bind(document);
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        let self = this; // guarda em uma variável o valor de this
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+        this._listaNegociacoes = new Bind(
+          new ListaNegociacoes(),
+          new NegociacoesView($('#negociacoesView')),
+          'adiciona', 'esvazia');
 
-            get(target, prop, receiver) {
-                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) === typeof(Function)) {
-                    return function() {
-                        console.log(`método '${prop}' interceptado`);
-                        Reflect.apply(target[prop], target, arguments);
-
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(target, prop, receiver);
-            }
-        });
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagem = new Bind(
+          new Mensagem(),
+          new MensagemView($('#mensagemView')),
+          'texto');
     }
 
     adiciona(event) {
-
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
-
         this._limpaFormulario();
     }
 
     apaga() {
-        // esvazia a lista
         this._listaNegociacoes.esvazia();
-
-        // exibe uma nova mensagem
         this._mensagem.texto = "Negociações removidas com sucesso";
-        // atualiza a view
-        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegociacao() {
         return new Negociacao(
           DateHelper.textoParaData(this._inputData.value),
           this._inputQuantidade.value,
-          this._inputValor.value
-        );
+          this._inputValor.value);
     }
 
     _limpaFormulario() {
         this._inputData.value = '';
-        this._inputData.focus();
         this._inputQuantidade.value = 1;
-        this._inputValor.value = 0.0
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
